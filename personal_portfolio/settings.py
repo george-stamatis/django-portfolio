b543c89f272d74440d9ad7e4115abc923ee9122d
george-stamatis <georgios.m.stamatis@gmail.com>
2024-03-19 18:55:04 +0100

    Hosting on Northflank

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l_o9gs(jqah-k5mj!f#vc@vsy*x(h*9y5xsqzlq==+ql6fk2yj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run']
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 
