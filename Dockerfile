5c10e102a2921de2216e787b3e1be16c72794bf5
george-stamatis <georgios.m.stamatis@gmail.com>
2024-03-20 14:21:53 +0100

    Secret key fix

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM python:3.12.1
+FROM python:3.12.2
 
 WORKDIR /app
 
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run']
+ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run', '127.0.0.1', 'localhost:8000']
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 
