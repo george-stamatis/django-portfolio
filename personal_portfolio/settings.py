674dc36bae3075af3e1970ca335deabfd0407642
george-stamatis <georgios.m.stamatis@gmail.com>
2024-03-21 03:08:22 +0100

    host fix

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key_here')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['p01--django--5bv8d5qycw54.code.run', '127.0.0.1', 'localhost']
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 
