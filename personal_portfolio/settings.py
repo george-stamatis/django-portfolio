1e68345c110e0f7f2b64631b8e852c13d2ec3195
george-stamatis <georgios.m.stamatis@gmail.com>
2024-03-21 02:44:48 +0100

    Docker fix

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
-from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
+from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('SECRET_KEY', 'your_default_secret_key'))
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run', '127.0.0.1', 'localhost:8000']
+ALLOWED_HOSTS = ['p01--django--fmd8y9b9q4tf.code.run', '127.0.0.1', 'localhost']
 
 MEDIA_ROOT = BASE_DIR / "uploads/"
 
@@ -48,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
